80080985a6828c5fd807b87594ff87f125c9c804
ibrahim <ibrahim.bas7@gmail.com>
2021-08-24 01:30:51 +0300

    ebobEkok

--- ebobEkok/src/com/company/Main.java
+++ ebobEkok/src/com/company/Main.java
@@ -7,17 +7,19 @@ public class Main {
     public static void main(String[] args) {
         int sayi1,sayi2;
         Scanner input = new Scanner(System.in);
-
+        //sayi alma
         System.out.print("Sayi 1 giriniz: ");
         sayi1 = input.nextInt();
         System.out.print("Sayi 2 giriniz: ");
         sayi2 = input.nextInt();
+        //sayi büyük küçük ayarlama
         if (sayi2>sayi1){
             int temp = sayi2;
             sayi2 = sayi1;
             sayi1 = temp;
         }
 
+        //ebob bulma
         int ebob = 1, sayac=sayi1;
 
         while (sayac>=0){
@@ -29,9 +31,11 @@ public class Main {
         }
         System.out.println("Ebob değeri : " + ebob);
 
+        //ebob ile ekok değeri bulma
         int ekok = sayi1*sayi2/ebob;
         System.out.println("Ekok değeri : " + ekok);
 
+        //ekok bulma
         sayac = 1;
         while (sayac<=sayi1*sayi2){
             if (sayac%sayi1 == 0 && sayac%sayi2 == 0){
